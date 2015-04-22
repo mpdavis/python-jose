bb07ad812f631289974dbe8d6be06daffb714e04
Michael Davis <mike.philip.davis@gmail.com>
2015-04-21 23:03:37 -0500

    Add setup files

---
+++ jose/__init__.py
@@ -0,0 +1,5 @@
+
+__version__ = "0.1.0"
+__author__ = 'Michael Davis'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2015 Michael Davis'
---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+import jose
+
+from setuptools import setup
+
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    long_description = readme.read()
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [
+        dirpath
+        for dirpath, dirnames, filenames in os.walk(package)
+        if os.path.exists(os.path.join(dirpath, '__init__.py'))
+    ]
+
+
+setup(
+    name='python-jose',
+    version=jose.__version__,
+    author='Michael Davis',
+    author_email='mike.philip.davis@gmail.com',
+    description='JOSE implementation in Python',
+    license='MIT',
+    keywords='jose jws jwe jwt json web token security signing',
+    url='http://github.com/mpdavis/jose',
+    packages=get_packages('jose'),
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+    ]
+)
