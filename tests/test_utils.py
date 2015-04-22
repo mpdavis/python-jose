b3d6ee2af517df8e1403494108c067ad3430e338
Michael Davis <mike.philip.davis@gmail.com>
2015-04-21 22:17:33 -0500

    Refactor timedelta_total_seconds

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,11 +5,10 @@ import unittest
 
 from jose import utils
 
+
 class UtilsTestCase(unittest.TestCase):
 
     def test_total_seconds(self):
         td = timedelta(seconds=5)
 
         self.assertEqual(utils.timedelta_total_seconds(td), 5)
-
-    
