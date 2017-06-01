d12fecd2eb012862b8d7654c879dccf5ccce833f
Gasper Zejn <zejn@kiberpipa.org>
2017-06-01 09:00:24 +0200

    Enable Python RSA backend as a fallback.

--- jose/backends/__init__.py
+++ jose/backends/__init__.py
@@ -2,7 +2,10 @@
 try:
     from jose.backends.pycrypto_backend import RSAKey
 except ImportError:
-    from jose.backends.cryptography_backend import CryptographyRSAKey as RSAKey
+    try:
+        from jose.backends.cryptography_backend import CryptographyRSAKey as RSAKey
+    except ImportError:
+        from jose.backends.rsa_backend import RSAKey
 
 try:
     from jose.backends.cryptography_backend import CryptographyECKey as ECKey
