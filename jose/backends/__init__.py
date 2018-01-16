402c815b7c9643896f66fe920f59ae6362088ca1
Michael Davis <mike.philip.davis@gmail.com>
2018-01-15 22:08:38 -0600

    default to pycrypto backend

--- jose/backends/__init__.py
+++ jose/backends/__init__.py
@@ -1,9 +1,9 @@
 
 try:
-    from jose.backends.cryptography_backend import CryptographyRSAKey as RSAKey
-except ImportError:
     from jose.backends.pycrypto_backend import RSAKey
-
+except ImportError:
+    from jose.backends.cryptography_backend import CryptographyRSAKey as RSAKey
+    
 try:
     from jose.backends.cryptography_backend import CryptographyECKey as ECKey
 except ImportError:
