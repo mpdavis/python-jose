9e55cd571560e6b91c4d2d668ea272e01c4defb4
Mickael Viey <mickael.viey@nerim.com>
2018-02-09 14:36:16 +0100

    python3 compatibility

--- tests/test_jwt.py
+++ tests/test_jwt.py
@@ -62,8 +62,8 @@ class TestJWT:
         encoded = jwt.encode(claims, key, headers=headers)
         assert encoded.split('.')[0] == b64encode(
             dumps(headers)
-                .replace(b' ', b'')
-                .encode('iso8859')).replace('==', '')
+                .replace(' ', '')
+                .encode('iso8859')).replace(b'==', b'')
 
     def test_encode(self, claims, key):
 
