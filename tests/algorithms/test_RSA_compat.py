eaa63837d50d86ac38a03c0cfd4ad38ea412482b
mattsb42-aws <bullocm@amazon.com>
2018-12-26 13:16:54 -0800

    expand RSA and ECDSA compatibility tests to validate serialization compatibility between backends

--- tests/algorithms/test_RSA_compat.py
+++ tests/algorithms/test_RSA_compat.py
@@ -3,23 +3,30 @@ import pytest
 try:
     from jose.backends.rsa_backend import RSAKey as PurePythonRSAKey
     from jose.backends.cryptography_backend import CryptographyRSAKey
-    from jose.backends.pycrypto_backend import RSAKey
+    from jose.backends.pycrypto_backend import RSAKey as PyCryptoRSAKey
 except ImportError:
-    PurePythonRSAKey = CryptographyRSAKey = RSAKey = None
+    PurePythonRSAKey = CryptographyRSAKey = PyCryptoRSAKey = None
 from jose.constants import ALGORITHMS
 
 from .test_RSA import private_key
 
+CRYPTO_BACKENDS = (
+    pytest.param(PurePythonRSAKey, id="python_rsa"),
+    pytest.param(CryptographyRSAKey, id="pyca/cryptography"),
+    pytest.param(PyCryptoRSAKey, id="pycrypto/dome")
+)
+ENCODINGS = ("PKCS1", "PKCS8")
+
 
 @pytest.mark.backend_compatibility
 @pytest.mark.skipif(
-    None in (PurePythonRSAKey, CryptographyRSAKey, RSAKey),
+    None in (PurePythonRSAKey, CryptographyRSAKey, PyCryptoRSAKey),
     reason="Multiple crypto backends not available for backend compatibility tests"
 )
 class TestBackendRsaCompatibility(object):
 
-    @pytest.mark.parametrize("BackendSign", [RSAKey, CryptographyRSAKey, PurePythonRSAKey])
-    @pytest.mark.parametrize("BackendVerify", [RSAKey, CryptographyRSAKey, PurePythonRSAKey])
+    @pytest.mark.parametrize("BackendSign", CRYPTO_BACKENDS)
+    @pytest.mark.parametrize("BackendVerify", CRYPTO_BACKENDS)
     def test_signing_parity(self, BackendSign, BackendVerify):
         key_sign = BackendSign(private_key, ALGORITHMS.RS256)
         key_verify = BackendVerify(private_key, ALGORITHMS.RS256).public_key()
@@ -33,18 +40,58 @@ class TestBackendRsaCompatibility(object):
         # invalid signature
         assert not key_verify.verify(msg, b'n' * 64)
 
-    @pytest.mark.parametrize("BackendFrom", [RSAKey, CryptographyRSAKey, PurePythonRSAKey])
-    @pytest.mark.parametrize("BackendTo", [RSAKey, CryptographyRSAKey, PurePythonRSAKey])
-    def test_public_key_to_pem(self, BackendFrom, BackendTo):
+    @pytest.mark.parametrize("encoding", ENCODINGS)
+    @pytest.mark.parametrize("BackendFrom", CRYPTO_BACKENDS)
+    @pytest.mark.parametrize("BackendTo", CRYPTO_BACKENDS)
+    def test_public_key_to_pem(self, BackendFrom, BackendTo, encoding):
         key = BackendFrom(private_key, ALGORITHMS.RS256)
-        pubkey = key.public_key()
+        key2 = BackendTo(private_key, ALGORITHMS.RS256)
 
-        pkcs1_pub = pubkey.to_pem(pem_format='PKCS1').strip()
-        pkcs8_pub = pubkey.to_pem(pem_format='PKCS8').strip()
-        assert pkcs1_pub != pkcs8_pub, BackendFrom
+        key1_pem = key.public_key().to_pem(pem_format=encoding).strip()
+        key2_pem = key2.public_key().to_pem(pem_format=encoding).strip()
+        assert key1_pem == key2_pem
 
-        pub1 = BackendTo(pkcs1_pub, ALGORITHMS.RS256)
-        pub8 = BackendTo(pkcs8_pub, ALGORITHMS.RS256)
+    @pytest.mark.parametrize("encoding", ENCODINGS)
+    @pytest.mark.parametrize("BackendFrom", CRYPTO_BACKENDS)
+    @pytest.mark.parametrize("BackendTo", CRYPTO_BACKENDS)
+    def test_private_key_to_pem(self, BackendFrom, BackendTo, encoding):
+        key = BackendFrom(private_key, ALGORITHMS.RS256)
+        key2 = BackendTo(private_key, ALGORITHMS.RS256)
 
-        assert pkcs8_pub == pub1.to_pem(pem_format='PKCS8').strip()
-        assert pkcs1_pub == pub8.to_pem(pem_format='PKCS1').strip()
+        key1_pem = key.to_pem(pem_format=encoding).strip()
+        key2_pem = key2.to_pem(pem_format=encoding).strip()
+
+        import base64
+        a = base64.b64decode(key1_pem[key1_pem.index(b"\n"):key1_pem.rindex(b"\n")])
+        b = base64.b64decode(key2_pem[key2_pem.index(b"\n"):key2_pem.rindex(b"\n")])
+        assert a == b
+
+        assert key1_pem == key2_pem
+
+    @pytest.mark.parametrize("encoding_save", ENCODINGS)
+    @pytest.mark.parametrize("encoding_load", ENCODINGS)
+    @pytest.mark.parametrize("BackendFrom", CRYPTO_BACKENDS)
+    @pytest.mark.parametrize("BackendTo", CRYPTO_BACKENDS)
+    def test_public_key_load_cycle(self, BackendFrom, BackendTo, encoding_save, encoding_load):
+        key = BackendFrom(private_key, ALGORITHMS.RS256)
+
+        pem_pub_reference = key.public_key().to_pem(pem_format=encoding_save).strip()
+        pem_pub_load = key.public_key().to_pem(pem_format=encoding_load).strip()
+
+        pubkey_2 = BackendTo(pem_pub_load, ALGORITHMS.RS256)
+
+        assert pem_pub_reference == pubkey_2.to_pem(encoding_save).strip()
+
+    @pytest.mark.parametrize("encoding_save", ENCODINGS)
+    @pytest.mark.parametrize("encoding_load", ENCODINGS)
+    @pytest.mark.parametrize("BackendFrom", CRYPTO_BACKENDS)
+    @pytest.mark.parametrize("BackendTo", CRYPTO_BACKENDS)
+    def test_private_key_load_cycle(self, BackendFrom, BackendTo, encoding_save, encoding_load):
+        key = BackendFrom(private_key, ALGORITHMS.RS256)
+
+        pem_reference = key.to_pem(pem_format=encoding_save).strip()
+        pem_load = key.to_pem(pem_format=encoding_load).strip()
+
+        key_2 = BackendTo(pem_load, ALGORITHMS.RS256)
+
+        assert pem_reference == key_2.to_pem(encoding_save).strip()
