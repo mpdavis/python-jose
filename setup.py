c25693392bb3d234792e5df0dceae8327c90909b
mattsb42-aws <bullocm@amazon.com>
2019-04-02 16:40:10 -0700

    add cryptography version constraint for PyPy < 5.4

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import platform
+
 import jose
 
 from setuptools import setup
@@ -21,9 +23,18 @@ def get_packages(package):
     ]
 
 
+def _cryptography_version():
+    # pyca/cryptography dropped support for PyPy < 5.4 in 2.5
+    # https://cryptography.io/en/latest/changelog/#v2-5
+    if platform.python_implementation() == 'PyPy' and platform.python_version() < '5.4':
+        return 'cryptography < 2.5'
+
+    return 'cryptography'
+
+
 pyasn1 = ['pyasn1']
 extras_require = {
-    'cryptography': ['cryptography'],
+    'cryptography': [_cryptography_version()],
     'pycrypto': ['pycrypto >=2.6.0, <2.7.0'] + pyasn1,
     'pycryptodome': ['pycryptodome >=3.3.1, <4.0.0'] + pyasn1,
 }
